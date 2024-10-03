b39c0927716315a72f4cf0209f82fbadf5189ae9
revatsai <revaqwertsai@gmail.com>
2024-10-03 22:54:12 +0800

    帳號功能 - 使用 MD5 加密密碼

--- src/main/java/com/revatsai/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/revatsai/springbootmall/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -35,6 +36,9 @@ public class UserServiceImpl implements UserService {
             log.warn("該 email {} 已經被註冊", userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+        // 使用MD5生成密碼的雜湊值
+        String hashPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes()); // 要將字串轉成Byte類型
+        userRegisterRequest.setPassword(hashPassword);
 
         // 創建帳號
         return userDao.createUser(userRegisterRequest);
@@ -44,12 +48,17 @@ public class UserServiceImpl implements UserService {
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
 
+        // 檢查user是否存在
         if (user == null) {
             log.warn("該 email {} 尚未註冊", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST); // 強制停止這次前端請求，Spring Boot回傳400狀態碼給前端，表示前端請求參數有問題
         }
 
-        if (user.getPassword().equals(userLoginRequest.getPassword())) {
+        // 使用MD5生成密碼的雜湊值
+        String hashPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        // 比較密碼
+        if (user.getPassword().equals(hashPassword)) {
             return user;
         } else {
             log.warn("email {} 的密碼不正確", userLoginRequest.getEmail());
